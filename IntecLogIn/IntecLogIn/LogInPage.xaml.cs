643f1305fe332878b656c5f38fa39143c85ebcc1
JPiantini98 <45038839+JPiantini98@users.noreply.github.com>
2020-11-22 21:43:09 -0400

    Project file upload

---
+++ IntecLogIn/IntecLogIn/LogInPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IntecLogIn
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LogInPage : TabbedPage
+    {
+        public LogInPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LogInClicked(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(userEmail.Text) || String.IsNullOrEmpty(userPassword.Text))
+            {
+                DisplayAlert("Error", "There is an empty field. Please complete it and try again", "OK");
+            }else
+            {
+                DisplayAlert("Welcome", "Nice to meet you " + userEmail.Text, "Thanks");
+            }
+        }
+
+        private void RegisterClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+        }
+    }
+}
---
+++ IntecLogIn/IntecLogIn/RegisterPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IntecLogIn
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterPage : MasterDetailPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        async void RegisterClicked(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(userName.Text) || String.IsNullOrEmpty(userEmail.Text) || String.IsNullOrEmpty(userPassword.Text) || String.IsNullOrEmpty(userPasswordVerify.Text))
+            {
+                await DisplayAlert("Error", "There is an empty field. Please complete it and try again", "OK");
+            }else if (userPassword.Text == userPasswordVerify.Text)
+            {
+                await DisplayAlert("Success!", "You had being succesfully registered", "OK");
+                await Navigation.PopAsync();
+            }else
+            {
+                await DisplayAlert("Error", "Password mismatch, please try againg", "OK");
+            }
+        }
+    }
+}
